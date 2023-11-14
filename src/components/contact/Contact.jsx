ebd7e6067af113c97abe8119331df3d1af6f8030
unknown <paulescumaria>
2023-11-14 20:48:28 +0200

    feat: Contact Page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import burger2 from "C:/Users/Ale/Desktop/webdevbasics/finalProject/Skillup_Burger_Shop/src/assets/burger2.png"
+import Popup from "reactjs-popup";
 
 function Contact () {
-    return (<></>)
+    return (
+        <div className="contact">
+            <form>
+                <h2>Contact Us</h2>
+                <input placeholder="Name"></input>
+                <input placeholder="Email"></input>
+                <textarea placeholder="Your message here..."></textarea>
+                <Popup trigger=
+                { <button>Send</button>}
+                >
+                    <div style={{color:"red", transform: 'translate(0%,-500%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Thank you for contacting us! We will get back to you shortly.</div>
+                </Popup>
+            </form>
+            <div className="formBorder">
+                <div>
+                    <img className="" src={burger2}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact;
