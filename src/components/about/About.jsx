c7801aaebbad26500d39381fe8bf908173a21a5d
unknown <paulescumaria>
2023-11-14 20:29:16 +0200

    feat: About page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import founder from "C:/Users/Ale/Desktop/webdevbasics/finalProject/Skillup_Burger_Shop/src/assets/skj.jpg"
+import { FaSearch } from "react-icons/fa";
+import Menu from "../home/Menu";
 function About () {
-    return (<></>)
+    const [show, setShow] = useState(false);
+
+    return (
+        show ?
+        <>
+            <Menu></Menu>
+        </>
+        :
+        <div className="founder">
+            <div>
+                <h3>Burger Shop</h3>
+                <p>This is Burger Shop. The place fot most tasty burgers on the enitre earth.</p>
+                <p>Explore the various type of food and burgers. Click below to see the menu</p>
+                <button className="explore" onClick={() => setShow(true)}><FaSearch /></button> 
+            </div>
+            <div>
+                <h2>Founder</h2>
+                <img src={founder} alt="founder"/>
+                <h3>Nelson</h3>
+                <p>i am Nelson, the founder of Burger Shop.</p>
+                <p>Affiliated to God Taste...</p>
+            </div>
+        </div>
+    )
 }
 
 export default About;
